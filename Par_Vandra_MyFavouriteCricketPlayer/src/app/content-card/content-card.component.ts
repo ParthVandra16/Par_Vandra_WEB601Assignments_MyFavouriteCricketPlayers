d679667914ab5a58f91fd0876661c8391fd4fbb8
ParthVandra16 <106701842+ParthVandra16@users.noreply.github.com>
2023-02-02 14:45:46 -0500

    genrate component list

--- Par_Vandra_MyFavouriteCricketPlayer/src/app/content-card/content-card.component.ts
+++ Par_Vandra_MyFavouriteCricketPlayer/src/app/content-card/content-card.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
+//import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,18 +8,5 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  CricketerListArray=new ContentList();
-  ngOnInit(){
 
-    const CricketOne:Content={
-      id: 1,
-    title: 'MS Dhoni',
-    description: 'Indian Cricket team captain',
-    cricketername: 'Virat Kohli'
-      //imageUrl:'https://angular.io/assets/images/logos/angular/angular.png',
-
-    }
-    this.CricketerListArray.AddCricketerFunction(CricketOne);
-
-  }
 }
